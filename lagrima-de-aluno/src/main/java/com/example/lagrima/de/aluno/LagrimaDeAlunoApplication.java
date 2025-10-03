fef7645e4c18276bd02b4012e9410c2ad9161380
Savio123d <savio13foxy@gmail.com>
2025-10-02 22:13:12 -0300

    spring boot

---
+++ lagrima-de-aluno/src/main/java/com/example/lagrima/de/aluno/LagrimaDeAlunoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lagrima.de.aluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagrimaDeAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagrimaDeAlunoApplication.class, args);
+	}
+
+}
---
+++ lagrima-de-aluno/src/test/java/com/example/lagrima/de/aluno/LagrimaDeAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lagrima.de.aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagrimaDeAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
